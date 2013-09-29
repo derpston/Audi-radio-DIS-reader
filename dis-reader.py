718c4315e256e005efab21d1d56e7eff8c0abaa6
derpston <derpston@sleepygeek.org>
2013-09-29 18:11:03 +0000

    Adding checksum validation, changing return value of getDISMessages to include the checksum validity.

--- dis-reader.py
+++ dis-reader.py
@@ -6,18 +6,34 @@ def chunk(iterable, chunksize):
     for index in xrange(0, len(iterable), chunksize):
         yield iterable[index : index + chunksize]
 
-def decode(message):
-    """Takes a list of integers representing bits and returns a string with the ASCII equivalent."""
-    output = ""
+def decode(bitstream):
+    """Takes a list of integers representing bits and returns (bool, str) representing the checksum validity and the string with the ASCII equivalent of the message."""
+    message = ""
 
-    # Read the message byte-by-byte, skipping the first and last ones
-    # which appear to be fixed
-    for byte in chunk(message[8:-8], 8):
-        # Convert each 8-bit list of integers to an ASCII character.
+    checksum = 0
+
+    # Read the message byte-by-byte, skipping the first which is a header.
+    for index, bits in enumerate(chunk(bitstream[:-8], 8)):
+        # Convert each 8-bit list of integers to an int.
         # (Ugly, sorry!)
-        output += chr(int("".join(map(lambda bit: str(bit), byte)), 2))
+        byte = int("".join(map(lambda bit: str(bit), bits)), 2)
+
+        # Update the checksum with this byte.
+        checksum += byte
+
+        # Append all non-header bytes to the message.
+        if index > 0:
+            message += chr(byte)
+    
+    # Read the transmitted checksum.
+    transmitted_checksum = int("".join(map(lambda bit: str(bit), bitstream[-8:])), 2)
+
+    # Verifying the checksum involves taking the sum of all bytes
+    # including the header and comparing the inverted least significant
+    # byte against the transmitted checksum.
+    checksum_validity= ((checksum & 0xff) ^ 0xff) == transmitted_checksum
         
-    return output
+    return (checksum_validity, message)
 
 def getDISMessages(binary_one_threshold = 20000):
     """A generator that attempts to read Audi radio DIS messages from
@@ -65,6 +81,7 @@ def getDISMessages(binary_one_threshold = 20000):
                 message.append(int(data > binary_one_threshold))
     
                 if len(message) == (18 * 8): # 18 bytes.
+                    # Found the end of the message.
                     yield decode(message)
                     message = None
     
@@ -73,7 +90,13 @@ def getDISMessages(binary_one_threshold = 20000):
             prev_clock = clock
 
 if __name__ == "__main__":
-    print "Waiting for DIS radio messages. (possible pyaudio error messages are expected)"
-    for message in getDISMessages():
-        print message
+    print "Waiting for DIS radio messages."
+    print "(possible pyaudio/alsa/pulse/jack/etc warnings are expected)"
+    for (checksum_valid, message) in getDISMessages():
+        print repr(message),
+        
+        if checksum_valid:
+            print "(checksum valid)"
+        else:
+            print "(checksum INVALID)"
 
